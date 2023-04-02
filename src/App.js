33d1765146a324e950c60d96ce25538c38d7cde9
Maciej Rybak <maciek.rybak95@gmail.com>
2023-04-02 17:44:28 +0200

    add store file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import React from "react";
+import { Provider } from "react-redux";
+
+import store from "./store/store";
+
 const App = () => {
-  return <p>Programowanie z Samurajem i Domanem :)</p>;
+  return;
+  <Provider store={store}>
+    <p>Programowanie z Samurajem i Domanem :)</p>;
+  </Provider>;
 };
 
 export default App;
